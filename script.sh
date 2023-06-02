3104849e9c0d6ae8d6f2b641952f49c208fd4dfe
Anna Samus <annapn32@gmail.com>
2023-06-02 10:26:05 +0100

    upload correct file

--- script.sh
+++ script.sh
@@ -14,3 +14,12 @@ touch 2_1.txt
 touch 2_2.txt
 touch 2_3.txt
 
+chmod 600 fol_1/*_1.txt fol_1/*_3.txt
+chmod 600 fol_2/*_1.txt fol_2/*_3.txt
+
+
+chmod 777 fol_1/*_2.txt
+chmod 777 fol_2/*_2.txt
+
+
+echo "Job completed"
